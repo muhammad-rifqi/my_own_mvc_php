fef5e19054c3a0d7b8e31261fce8bfe45891c13a
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-03-19 11:58:33 +0700

    aaa

--- index.php
+++ index.php
@@ -5,15 +5,15 @@ include "database.php";
 include "controller.php";
 
 if($_GET['menu'] == ''){
-     $data['title'] = "Web Title"; 
+     $data['title'] = "Home Title"; 
      return Controller::views('home', $data);
 } if($_GET['menu'] == 'profile'){
-     $data['title'] = "Web Title"; 
+     $data['title'] = "Profile Title"; 
      $data['list'] = Controller::model('user')->getallemployee();
      return Controller::views('profile', $data);
 } if($_GET['menu'] == 'detail_profile'){
      $data['id'] = $_GET['id'];
-     $data['title'] = "Web Title"; 
+     $data['title'] = "Profile Detail Title"; 
      $data['list'] = Controller::model('user')->getdetailemployee($data['id']);
      return Controller::views('profile', $data);
 }
